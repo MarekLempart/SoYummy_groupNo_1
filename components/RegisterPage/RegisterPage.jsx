b66ff322c5eb9867f39151dca43ddb35100bad59
kruzelgo <kruzelgo@gmail.com>
2024-08-02 09:53:45 +0200

    renaming files
    - add AuthForm.jsx
    -changing RegiterPage.css to AuthForm.css

---
+++ components/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import AuthForm from "./AuthForm";
+
+const RegisterPage = () => {
+  return (
+    <div className="register-page">
+      <AuthForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
