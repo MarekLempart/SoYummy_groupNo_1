13d38234068564f906975f56be0568eb83165e32
kruzelgo <kruzelgo@gmail.com>
2024-08-03 13:44:59 +0200

    new txt in h1 and p

--- components/WelcomePage/WelcomePage.js
+++ components/WelcomePage/WelcomePage.js
@@ -5,8 +5,12 @@ import styles from "./WelcomePage.module.css";
 const WelcomePage = () => {
   return (
     <div className={styles.container}>
-      <h1>Welcome to SoYummy</h1>
-      <p>Please register or sign in to continue.</p>
+      <h1>Welcome to the app!</h1>
+      <p>
+        This app offers more than just a collection of recipes - it is designed
+        to be your very own digital cookbook. You can easily save and retrieve
+        your own recipes at any time..
+      </p>
       <AuthNav />
     </div>
   );
